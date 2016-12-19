ab3820b244b605e07b74c0c4791f7353c7f909f1
blabla4 <amaury.cannesson@gmai.com>
2016-12-19 22:50:20 +0100

    module1

---
+++ module1/app.js
@@ -0,0 +1,22 @@
+(function() {
+  var app = angular.module("LunchCheck", []);
+  app.controller("LunchCheckController", LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+    $scope.check = function() {
+      if($scope.lunchMenu == undefined || !($scope.lunchMenu.length)) {
+        $scope.result = "Please enter data first";
+        $scope.alertStyle = "alert-info";
+      }
+      if($scope.lunchMenu.split(',').length > 3) {
+        $scope.result = "Too much!";
+        $scope.alertStyle = "alert-danger";
+      } else {
+        $scope.result = "Enjoy!";
+        $scope.alertStyle = "alert-success";
+      }
+    }
+  }
+})()
